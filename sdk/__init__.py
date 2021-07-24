8649d50aa697e49247fa4108e18f102b30398759
olufekosamuel <olufekosamuel@gmail.com>
2021-07-25 00:54:48 +0100

    add method for creating user token

---
+++ sdk/__init__.py
@@ -0,0 +1,26 @@
+import requests
+
+BASE_URL = "https://robbin-api.herokuapp.com/api/v1"
+
+class Robin:
+
+    def __init__(self, api_key, tls):
+
+        self.api_key = api_key
+        self.tls = tls
+
+    
+    def create_user_token(self, data):
+        # defining a params dict for the parameters to be sent to the API
+        DATA = data
+        print(data)
+        HEADERS = {'content-type': 'application/json', 'x-api-key': self.api_key}
+        
+        # sending post request and saving the response as response object
+        r = requests.post(url = BASE_URL+"/chat/user_token", data=DATA, headers=HEADERS)
+        
+        # extracting data in json format
+        data = r.json()
+
+        #return data
+        return data
---
+++ tests/create_user_token.py
@@ -0,0 +1,21 @@
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+import unittest
+from sdk import Robin
+
+
+robin_test = Robin("NT-LSTTNiKdEQyAagVBdhKtoqqTEhbXGGZxaQbp", True)
+
+class BaseCase(unittest.TestCase):
+    def test_create_user_token_success(self):
+        actual = robin_test.create_user_token(data={
+           "meta_data":{
+                "name": "Samuel",
+            }
+        })
+        print(actual)
+        expected = False
+        self.assertEqual(actual["error"], expected)
+
+if __name__ == '__main__':
+    unittest.main()
