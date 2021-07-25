6d40e2c8d5f44860287e535d34a7e723c948aa06
olufekosamuel <olufekosamuel@gmail.com>
2021-07-25 17:56:34 +0100

    fixed json issue

--- sdk/__init__.py
+++ sdk/__init__.py
@@ -13,11 +13,11 @@ class Robin:
     def create_user_token(self, data):
         # defining a params dict for the parameters to be sent to the API
         DATA = data
-        print(data)
+        print(DATA)
         HEADERS = {'content-type': 'application/json', 'x-api-key': self.api_key}
         
         # sending post request and saving the response as response object
-        r = requests.post(url = BASE_URL+"/chat/user_token", data=DATA, headers=HEADERS)
+        r = requests.post(url = BASE_URL+"/chat/user_token", json=DATA, headers=HEADERS)
         
         # extracting data in json format
         data = r.json()
