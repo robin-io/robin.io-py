92849aa0bd9054d7b0f4cc2d53afd0c45d575eaa
olufekosamuel <olufekosamuel@gmail.com>
2021-07-26 14:45:02 +0100

    added create conversation method

---
+++ tests/conversation_test.py
@@ -0,0 +1,16 @@
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+import unittest
+from sdk import Robin
+
+
+robin_test = Robin("NT-LSTTNiKdEQyAagVBdhKtoqqTEhbXGGZxaQbp", True)
+
+class BaseCase(unittest.TestCase):
+    def test_create_single_conversation_success(self):
+        actual = robin_test.create_conversation(sender_token="IZiawwHPpHeE", sender_name="Elvis Mike", receiver_token="IZiawwHPpHeE", receiver_name="Ayo Onas")
+        print(actual)
+        self.assertIsNotNone(actual)
+
+if __name__ == '__main__':
+    unittest.main()
